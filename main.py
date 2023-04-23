453758b1269bb5ae06b00d4989cf1ec08be4067a
Walid <walid.tawfik200@gmail.com>
2023-04-23 20:34:34 +0200

    commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from urllib.request import urlopen
 import json
 import requests
 from dotenv import load_dotenv
-# from object_detection import obejectDetection
+from object_detection import obejectDetection
 
 global embed
 
